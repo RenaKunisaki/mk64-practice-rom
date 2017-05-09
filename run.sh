a123c0d54766a04c5ff14617c7fa5dc4e5a9ed3c
RenaKunisaki <hyperhacker@gmail.com>
2017-05-09 19:05:17 -0400

    add run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# for Everdrive64 v3
+#./include/n64/tools/loader64 -w -f patched.rom
+#./include/n64/tools/loader64 -p
+
+# for 64drive
+64drive -vvl patched.rom
+
+# for mupen64 (XXX)
+#mupen64plus --corelib ~/projects/emus/mupen64plus/mupen64plus-core/projects/unix/libmupen64plus.so.2 --emumode 0 patched.rom
