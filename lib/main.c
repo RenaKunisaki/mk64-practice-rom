a36b1bc95d438b11d6eead61f7e241fd8a66a0c8
RenaKunisaki <hyperhacker@gmail.com>
2017-05-10 12:58:56 -0400

    add printNumInBase

--- lib/main.c
+++ lib/main.c
@@ -10,8 +10,34 @@ char* strAppend(char *dest, const char *src) {
 }
 
 
+char* printNumInBase(char *buf, u32 num, u32 base) {
+    //print unsigned integer into buffer.
+    //base can be up to 36; higher will work but use strange characters.
+    //returns pointer to null terminator.
+    if(base < 2) base = 10;
+    char digits[34];
+    char *d = &digits[33];
+    *d-- = '\0';
+    while(num > 0 || d == &digits[32]) { //print digits into buffer
+        char c = num % base;
+        if(c > 9) c = 'A' + (c-10);
+        else c = '0' + c;
+        *d-- = c;
+        num /= base;
+    }
+
+    //copy digits to buf
+    do {
+        *buf = *++d;
+    } while(*buf++);
+
+    return --buf;
+}
+
+
 char* printHex(char *buf, u32 num, int nDigits) {
     //print hex number into buffer.
+    //will zero-pad to specified number of digits.
     //will truncate numbers larger than specified length.
     //returns pointer to null terminator.
     char *bufEnd = &buf[nDigits];
@@ -27,20 +53,7 @@ char* printHex(char *buf, u32 num, int nDigits) {
 char* printNum(char *buf, u32 num) {
     //print decimal number into buffer.
     //returns pointer to null terminator.
-    char digits[16];
-    char *d = &digits[15];
-    *d-- = '\0';
-    while(num > 0 || d == &digits[14]) {
-        *d-- = hex[num % 10];
-        num /= 10;
-    }
-
-    do {
-        *buf = *++d;
-    } while(*buf++);
-
-    // *buf = 0;
-    return --buf;
+    return printNumInBase(buf, num, 10);
 }
 
 } //extern "C"
