499f151fc7fd2900f11651067155f2e51124b060
RenaKunisaki <hyperhacker@gmail.com>
2017-05-18 17:07:48 -0400

    include: MORE STUFF

--- include/mk64/controls/controls.h
+++ include/mk64/controls/controls.h
@@ -3,6 +3,8 @@ typedef struct {
     u16 buttons;
 } controllerState;
 
+extern void _InitControllers();
+extern void readControllers();
 extern controllerState controller1_state; //XXX others?
 extern controllerState *controllers[4]; //XXX are all 4 here?
 
--- include/mk64/video/gfx.h
+++ include/mk64/video/gfx.h
@@ -1,3 +1,9 @@
+#define SCREEN_WIDTH 320
+#define SCREEN_HEIGHT 240
+#define SCREEN_SIZE (SCREEN_WIDTH * SCREEN_HEIGHT)
+
+#define RGB16(r, g, b) (((r) << 11) | ((g) << 6) | ((b) << 1) | 1)
+
 extern struct {
     struct { u16 r, g, b; } top, bottom; //why uint16_t? no idea.
     //upper bytes seem to be used for something...
@@ -13,6 +19,8 @@ enum {
 extern int screenMode, screenSplitMode;
 extern u32* dlistBuffer; //ptr to current position in buffer
 extern u16* frameBuffers[4]; //XXX verify size
+extern u16 frameBuffer0[SCREEN_SIZE],
+    frameBuffer1[SCREEN_SIZE], frameBuffer2[SCREEN_SIZE];
 extern s16 curDisplayList1; //XXX size, players
 extern s16 curDisplayList2; //copied here
 
